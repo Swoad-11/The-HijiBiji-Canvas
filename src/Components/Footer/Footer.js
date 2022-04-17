b34b732eaf2c4c6727243d58311e0058f3cb49f3
Swoad-11 <toufiq11swoad@gmail.com>
2022-04-17 17:56:14 +0600

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
-        <div className='mt-12'>
-            <footer>
-                <p>The HijiBiji Canvas</p>
-                <div class="social">
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
+        <div className='mt-12 bg-blue-200'>
+            <footer className='text-center'>
+                <p className='text-2xl pt-4 font-semibold text-white'>The HijiBiji Canvas</p>
+                <div className="pt-2 pb-2">
+                    <a className='social px-2 text-2xl' href="https://www.facebook.com/dHJBJcanvas"><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon></a>
+                    <a className='social px-2 text-2xl' href="https://www.instagram.com/thehijibijicanvas/"><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></a>
                 </div>
-                <p class="end">CopyRight By Tahmid Ahmed</p>
+                <p className='pb-4 text-xs text-black'>&copy; By The HijiBiji Canvas 2022</p>
             </footer>
         </div>
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,11 +10,11 @@ const Home = () => {
                 <div className='grid grid-cols-2 items-center text-center justify-items-start'>
                     <img className='mx-auto' src="hijibiji1.png" alt="" />
                     <div className='mr-24'>
-                        <h1 className='text-6xl font-bold'>The HijiBiji Canvas</h1>
-                        <p className='text-2xl'>
+                        <h1 className='title text-6xl font-bold'>The HijiBiji Canvas</h1>
+                        <p className='description text-2xl text-teal-600'>
                             Here you will find digital illustrations, pencil sketches,canvas paintings and more!
                         </p>
-                        <button className="px-4 py-2 font-semibold text-sm text-white bg-amber-600 hover:bg-amber-700 rounded-lg mt-4">
+                        <button className="px-4 py-2 font-semibold text-sm text-white bg-teal-500 hover:bg-teal-700 rounded-lg mt-4">
                             <Link to="/service">Our Services</Link>
                         </button>
                     </div>
