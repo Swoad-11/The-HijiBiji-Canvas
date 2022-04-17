0a6758d0876d7eb524279a0d0a88d0ea968a9501
Swoad-11 <toufiq11swoad@gmail.com>
2022-04-17 11:36:42 +0600

    firebase,router installed and components created

---
+++ src/Service/Servive.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Servive = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Servive;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCEKsGu344FWuAVuEDX-jFa5BTt0ZVSJLc",
+    authDomain: "hijibiji-canvas-11.firebaseapp.com",
+    projectId: "hijibiji-canvas-11",
+    storageBucket: "hijibiji-canvas-11.appspot.com",
+    messagingSenderId: "754967116103",
+    appId: "1:754967116103:web:30c995cccffd70d207f462"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
